34cb3344523a600e8570ac19b8cdf3bf33a355f2
Eduardo <duuh-l@hotmail.com>
2018-10-24 00:33:31 -0300

    Criando controllers User e traits Crud Methods

---
+++ app/controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Duh
+ * Date: 24/10/2018
+ * Time: 01:26
+ */
+
+namespace Controllers;
+
+
+use Traits\CrudMethods;
+
+class UserController
+{
+    use CrudMethods;
+
+    public function find($id)
+    {
+        
+    }
+
+
+}//end class
--- app/controllers/traits/CrudMethods.php
+++ app/controllers/traits/CrudMethods.php
@@ -6,9 +6,18 @@
  * Time: 01:23
  */
 
+namespace Traits;
+
 use config\Conexao;
 
 trait CrudMethods
 {
+    /**
+     * Function find By ID
+     * @param $id
+     */
+    public function find($id)
+    {
 
+    }
 }
